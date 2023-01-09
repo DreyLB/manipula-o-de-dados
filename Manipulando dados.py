209b987e865e504b6ac039d3d3494e2fd5f026fa
Andrey Cesar <andreycesar02@gmail.com>
2023-01-08 21:57:04 -0300

    primeiro commit

---
+++ Manipulando dados.py
@@ -0,0 +1,21 @@
+import csv
+
+def abrir_leitura(caminho):
+    with open(caminho, 'r') as arquivo:
+        abrir = csv.reader(arquivo)
+        dados = list(abrir)
+        arquivo.close()
+        return dados
+
+
+caminho = 'E:\VS Code Python\Manipulação de Arquivos\salarios.txt'
+dados = abrir_leitura(caminho)
+tamanho = len(dados)
+for c in range(100):
+    print('\n')
+    for i in range(4):
+        print(f'{dados[0][i]}: {dados[c][i]}')
+
+
+
+
